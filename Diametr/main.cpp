0b90a9f27485a91648aa71de1a990e0a08af0b89
MospanMyroslav <mrhomaster@gmail.com>
2013-06-02 18:22:50 +0300

    task 2

---
+++ Diametr/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std; 
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+   int radius; 
+   cout << "Введите радиус окружности:  ";
+   cin >> radius; 
+
+   cout << "Диаметр  " << radius * 2.0 << "\n Окружность " << 2 * 3.14159 * radius
+      << "\n Площадь равна " << 3.14159 * radius * radius << endl;
+     system("pause");
+}
